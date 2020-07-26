6059a97173b37271a9deb5f65bd6168308570570
Bart Dority <b0rgBart3@gmail.com>
2020-07-25 17:04:12 -0700

    preparing for deployment

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+var host = process.env.HOST || "localhost";
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
